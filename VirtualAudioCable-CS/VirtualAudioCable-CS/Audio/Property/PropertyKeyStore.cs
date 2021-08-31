7708afc1415592ad7cefc9923d31a3d4f7a9cc1e
EinfachEinAlex <mitteodersoyt@gmail.com>
2021-08-31 10:33:52 +0200

    Fix for the Windows 21H1 update

--- VirtualAudioCable-CS/VirtualAudioCable-CS/Audio/Property/PropertyKeyStore.cs
+++ VirtualAudioCable-CS/VirtualAudioCable-CS/Audio/Property/PropertyKeyStore.cs
@@ -5,18 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VirtualAudioCable_CS.Audio.Struct;
+using VirtualAudioCable_CS.Utils;
+using VirtualAudioCable_CS.Utils.Version;
 
 namespace VirtualAudioCable_CS.Audio.Property
 {
     class PropertyKeyStore
     {
-        public static PropertyKey SET_PRIMARY_AUDIO_LOOPBACK_DEVICES = new PropertyKey(new Guid("24dbb0fc-9311-4b3d-9cf0-18ff155639d4"), 1);
+        public PropertyKeyStore()
+        {
+            if (Windows.GetWindowsDisplayVersion().Equals("21H1"))
+            {
+                DEVICE_INTERFACE_FRIENDLY_NAME = new PropertyKey(new Guid("b3f8fa53-0004-438e-9003-51a46e139bfc"), 6);
+            }
+        }
 
-        public static PropertyKey AUDIO_LOOPBACK_DEVICES = new PropertyKey(new Guid("24dbb0fc-9311-4b3d-9cf0-18ff155639d4"), 0);
+        public PropertyKey SET_PRIMARY_AUDIO_LOOPBACK_DEVICES { get; } = new PropertyKey(new Guid("24dbb0fc-9311-4b3d-9cf0-18ff155639d4"), 1);
 
-        public static PropertyKey DEVICE_INTERFACE_FRIENDLY_NAME = new PropertyKey(new Guid("026E516E-B814-414B-83CD-856D6FEF4822"), 2);
+        public PropertyKey AUDIO_LOOPBACK_DEVICES { get; } = new PropertyKey(new Guid("24dbb0fc-9311-4b3d-9cf0-18ff155639d4"), 0);
 
-        public static PropertyKey AUDIO_ENDPOINT_GUID = new PropertyKey(new Guid("1DA5D803-D492-4EDD-8C23-E0C0FFEE7F0E"), 4);
+        public PropertyKey DEVICE_INTERFACE_FRIENDLY_NAME { get; } = new PropertyKey(new Guid("026E516E-B814-414B-83CD-856D6FEF4822"), 2);
+
+        public PropertyKey AUDIO_ENDPOINT_GUID { get; } = new PropertyKey(new Guid("1DA5D803-D492-4EDD-8C23-E0C0FFEE7F0E"), 4);
 
     }
 }
---
+++ VirtualAudioCable-CS/VirtualAudioCable-CS/Utils/Version/Windows.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualAudioCable_CS.Utils.Version
+{
+    class Windows
+    {
+        public static string GetWindowsDisplayVersion()
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+            object dV = key.GetValue("DisplayVersion");
+
+            if (dV != null)
+            {
+                if (dV is string)
+                {
+                    return dV.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
